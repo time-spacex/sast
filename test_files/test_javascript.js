39733dc9349a0a2a0e573e85ad828de815cb8f76
r.kleshnev <r.kleshnev@ddos-guard.net>
2024-08-23 15:50:54 +0300

    delete test files

--- test_files/test_javascript.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/vulnerable', (req, res) => {
-    // Cross-Site Scripting (XSS) vulnerability
-    res.send(`<h1>${req.query.userInput}</h1>`);
-});
-
-app.listen(3000);
